119292f4b1224cb4b16eb99823de02e0a01feda9
scremin <azflresc@hotmail.com>
2020-12-26 21:49:04 +0000

    add col t3

--- app-t1/src/components/3-cols-main.vue
+++ app-t1/src/components/3-cols-main.vue
@@ -50,11 +50,13 @@
     flex-wrap: wrap;
     align-content: center;
 
-    height: 100%;
-    width: 100%;
+    height: 90%;
+    width: 90%;
 
     border: solid green;
     /*background: linear-gradient(to right, rgb(8, 80, 120), rgb(133, 216, 206));*/
+
+    box-shadow: 0 2px 10px rgba(220, 240, 240, 0.35);
 }
 
 .classe1 {
@@ -62,7 +64,7 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    align-content:space-between;
+    align-content:space-around;
 
     height: 90%;
     width: 25%;
